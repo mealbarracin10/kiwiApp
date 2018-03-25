21a1f85f2f7774541880d164ab71f22b6bcc6c12
Martin  Albarracin <edgar3280@gmail.com>
2018-03-24 19:21:10 -0500

    continue integration form map box,  -  Characteristic 	- found location destiny         - trace route into source and destiny

--- src/components/Directions.js
+++ src/components/Directions.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, MapView } from 'react-native';
+import { View, StyleSheet, MapView, Alert } from 'react-native';
 import MapboxGL from '@mapbox/react-native-mapbox-gl';
 import MapboxClient from 'mapbox';
 import Constants from '../helpers/constants.js';
+import { lineString } from '@turf/helpers';
 
+const layerStyles = MapboxGL.StyleSheet.create({
+    origin: {
+        circleRadius: 5,
+        circleColor: 'white',
+    },
+    destination: {
+        circleRadius: 5,
+        circleColor: 'white',
+    },
+    route: {
+        lineColor: 'red',
+        lineWidth: 3,
+        lineOpacity: 0.84,
+    },
+    progress: {
+        lineColor: '#314ccd',
+        lineWidth: 3,
+    },
+});
 
 export default class Directions extends Component {
 
-    getDirections(src, dest) {
-        var mapboxClient = new MapboxClient(Constants.mapboxAccessToken);
+    constructor(props) {
+        super(props);
+        this.state = {
+            mapboxClient: null,
+            route: null,
+        };
+    }
+
+    async componentDidMount() {
+
+        try {
+            this.getDirections(this.props.source, this.props.destiny);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    async getDirections(src, dest) {
 
         const sourcelatlon = {
             latitude: src[1],
@@ -25,42 +61,48 @@ export default class Directions extends Component {
             geometry: 'polyline',
             steps: true,
         };
+        let responseDirections = null;
 
-        console.log("source")
-        console.log(sourcelatlon)
-        console.log("destiny")
-        console.log(destlatLng)
-        console.log("options")
-        console.log(requestOptions)
-
-        res = mapboxClient.getDirections([
-            sourcelatlon,
-            destlatLng
-        ], requestOptions);
-
-        console.log(res.routes)
-
-    }
-
-    async componentDidMount() {
-        
         try {
-            this.getDirections(this.props.source, this.props.destiny);
-        } catch (e) {
-            console.log(e);
+            var client = new MapboxClient(Constants.mapboxAccessToken)
+            responseDirections = await client.getDirections([
+                sourcelatlon,
+                destlatLng,
+            ], requestOptions);
+
+        } catch (error) {
+            console.log("errores -------")
+            console.log(error);
         }
+
+        if (responseDirections == null) {
+            return;
+        }
+
+        if (!(responseDirections.entity.routes)) {
+            Alert.alert("Can't find a way there");
+              return
+        }
+
+        const directions = responseDirections.entity.routes[0];
+        const distance = directions.distance;
+        const duration = directions.duration;
+        const steps = directions.legs[0].steps;
+        this.setState({
+            route: lineString(directions.geometry.coordinates)
+        });
+
     }
+
 
     render() {
+        if (!this.state.route) {
+            return null;
+        }
         return (
-            <View>
-            </View>
-            //   <MapboxGL.ShapeSource id='mapbox-directions-source' shape={this.state.directions.geometry}>
-            //     <MapboxGL.LineLayer
-            //       id='mapbox-directions-line'
-            //       belowLayerID={Places.UnselectedSymbolID}
-            //       style={[styles.directionsLine, this.props.style]} />
-            //   </MapboxGL.ShapeSource>
+            <MapboxGL.ShapeSource id='routeSource' shape={this.state.route}>
+                <MapboxGL.LineLayer id='routeFill' style={layerStyles.route} belowLayerID='place-city-sm' />
+            </MapboxGL.ShapeSource>
         );
     }
 
